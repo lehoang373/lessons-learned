4924917986fc61324040773907bd889294ccf471
le hoang <lehoang@les-MacBook-Pro.local>
2020-05-06 13:15:26 -0500

    first commit

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/lehoang/Desktop/ll working final/lessonslearned/myvenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
